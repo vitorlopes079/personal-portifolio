68a84b87ce43831e06f54927433963cc6732cabe
vitorlopes079 <vitorlopes079@gmail.com>
2024-01-18 15:05:58 +0700

    implemented images slider animaitons

--- src/component/ImageSlider.js
+++ src/component/ImageSlider.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
-
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
 
 const ImageSlider = ({ images, isLargeScreen }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const {t} = useTranslation()
+  const { t } = useTranslation();
   const goToPreviousImage = () => {
     const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
@@ -18,27 +16,40 @@ const ImageSlider = ({ images, isLargeScreen }) => {
   };
 
   return (
-    <div className="hover-parent relative responsive1000:w-2/4 lg:w-3/5 p-4 flex flex-col justify-center items-center">
-      <FontAwesomeIcon
-        icon={faAngleLeft}
-        onClick={goToPreviousImage}
-        className="arrow-left text-gray-500 text-4xl cursor-pointer z-10"
-      />
-      <img
-        src={images[currentIndex]}
-        className="border-1 border-gray-200 "
-        alt={`Slide ${currentIndex + 1}`}
-      />
-      <FontAwesomeIcon
-        icon={faAngleRight}
-        onClick={goToNextImage}
-        className="arrow-right text-gray-500 text-4xl cursor-pointer z-10"
-      />
-      <div>
+    <div className="responsive1000:w-2/4 lg:w-3/5 ">
+      <div className="relative w-full h-full">
+        <button
+          className="slider-button left-0 z-10 p-2"
+          onClick={goToPreviousImage}
+        >
+          <ArrowBigLeft />
+        </button>
+        <div className="border-1 border-gray-200 w-full h-full overflow-hidden flex">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Slide ${index + 1}`} 
+              className="object-cover w-full h-full block"
+              style={{
+                transform: `translateX(${-100 * currentIndex}%)`,
+                transition: "transform 300ms ease-in-out",
+              }}
+            />
+          ))}
+        </div>
+
+        <button
+          className="slider-button right-0 z-10 p-2"
+          onClick={goToNextImage}
+        >
+          <ArrowBigRight />
+        </button>
+        
+      </div>
+      <div className="flex justify-center">
         {isLargeScreen && (
-          <p className="text-sm text-red-600 pt-2">
-            {t("See_the_website")}
-          </p>
+          <p className="text-sm text-red-600 pt-2">{t("See_the_website")}</p>
         )}
       </div>
     </div>
